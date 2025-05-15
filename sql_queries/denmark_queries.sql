09ae1268e63cbdffd4145673b8ef259d7f7ae80a
montMateus <montmateus.costa05@gmail.com>
2025-05-15 14:28:32 -0300

    Adição de análises

--- sql_queries/denmark_queries.sql
+++ sql_queries/denmark_queries.sql
@@ -31,7 +31,7 @@ GROUP BY EXTRACT(YEAR FROM datetime_utc)
 SELECT * FROM a
 ORDER BY extract
 
--- SUM HOURLY
+-- Visão geral
 
 WITH dk_2021_summary AS (
     SELECT 
@@ -119,4 +119,171 @@ SELECT
     avg_renewable_percentage,
     avg_low_carbon_percentage
 FROM with_differences
-ORDER BY year_, month_grouped;
+ORDER BY year_, month_grouped
+
+-- Emissão por ano
+
+WITH dk_2021_summary AS (
+    SELECT 
+        '2021' AS year_,
+        country,
+        zone_id,
+        EXTRACT(MONTH FROM datetime_utc) AS month_grouped,
+        ROUND(SUM(carbon_intensity_direct)::NUMERIC, 2) AS total_direct_carbon_emission,
+        ROUND(SUM(carbon_intensity_lca)::NUMERIC, 2) AS total_lca_carbon_emission,
+        ROUND(AVG(renewable_percentage)::NUMERIC, 2) AS avg_renewable_percentage,
+        ROUND(AVG(low_carbon_percentage)::NUMERIC, 2) AS avg_low_carbon_percentage
+    FROM denmark.year_2021_hourly
+    GROUP BY country, zone_id, EXTRACT(MONTH FROM datetime_utc)
+), dk_2022_summary AS (
+    SELECT 
+        '2022' AS year_,
+        country,
+        zone_id,
+        EXTRACT(MONTH FROM datetime_utc) AS month_grouped,
+        ROUND(SUM(carbon_intensity_direct)::NUMERIC, 2) AS total_direct_carbon_emission,
+        ROUND(SUM(carbon_intensity_lca)::NUMERIC, 2) AS total_lca_carbon_emission,
+        ROUND(AVG(renewable_percentage)::NUMERIC, 2) AS avg_renewable_percentage,
+        ROUND(AVG(low_carbon_percentage)::NUMERIC, 2) AS avg_low_carbon_percentage
+    FROM denmark.year_2022_hourly
+    GROUP BY country, zone_id, EXTRACT(MONTH FROM datetime_utc)
+), dk_2023_summary AS (
+    SELECT 
+        '2023' AS year_,
+        country,
+        zone_id,
+        EXTRACT(MONTH FROM datetime_utc) AS month_grouped,
+        ROUND(SUM(carbon_intensity_direct)::NUMERIC, 2) AS total_direct_carbon_emission,
+        ROUND(SUM(carbon_intensity_lca)::NUMERIC, 2) AS total_lca_carbon_emission,
+        ROUND(AVG(renewable_percentage)::NUMERIC, 2) AS avg_renewable_percentage,
+        ROUND(AVG(low_carbon_percentage)::NUMERIC, 2) AS avg_low_carbon_percentage
+    FROM denmark.year_2023_hourly
+    GROUP BY country, zone_id, EXTRACT(MONTH FROM datetime_utc)
+), dk_2024_summary AS (
+    SELECT 
+        '2024' AS year_,
+        country,
+        zone_id,
+        EXTRACT(MONTH FROM datetime_utc) AS month_grouped,
+        ROUND(SUM(carbon_intensity_direct)::NUMERIC, 2) AS total_direct_carbon_emission,
+        ROUND(SUM(carbon_intensity_lca)::NUMERIC, 2) AS total_lca_carbon_emission,
+        ROUND(AVG(renewable_percentage)::NUMERIC, 2) AS avg_renewable_percentage,
+        ROUND(AVG(low_carbon_percentage)::NUMERIC, 2) AS avg_low_carbon_percentage
+    FROM denmark.year_2024_hourly
+    GROUP BY country, zone_id, EXTRACT(MONTH FROM datetime_utc)
+), total_summary AS (
+    SELECT * FROM dk_2021_summary
+    UNION ALL
+    SELECT * FROM dk_2022_summary
+    UNION ALL
+    SELECT * FROM dk_2023_summary
+    UNION ALL
+    SELECT * FROM dk_2024_summary
+), with_differences AS (
+    SELECT 
+        *,
+        LEAD(total_direct_carbon_emission) OVER (PARTITION BY country, zone_id ORDER BY year_, month_grouped) AS next_direct_carbon_emission,
+        LEAD(total_lca_carbon_emission) OVER (PARTITION BY country, zone_id ORDER BY year_, month_grouped) AS next_lca_carbon_emission
+    FROM total_summary
+)
+
+SELECT 
+    year_,
+    country,
+    SUM(total_direct_carbon_emission) AS total_direct_carbon_emission,
+    SUM(total_lca_carbon_emission) AS total_lca_carbon_emission
+FROM with_differences
+GROUP BY year_, country
+ORDER BY total_direct_carbon_emission DESC
+
+-- Em quais horas do dia há mais emissão?
+
+WITH dk_2021_summary AS (
+    SELECT 
+        '2021' AS year_,
+        country,
+        zone_id,
+        EXTRACT(HOUR FROM datetime_utc) AS hour_grouped,
+        ROUND(SUM(carbon_intensity_direct)::NUMERIC, 2) AS total_direct_carbon_emission,
+        ROUND(SUM(carbon_intensity_lca)::NUMERIC, 2) AS total_lca_carbon_emission,
+        ROUND(AVG(renewable_percentage)::NUMERIC, 2) AS avg_renewable_percentage,
+        ROUND(AVG(low_carbon_percentage)::NUMERIC, 2) AS avg_low_carbon_percentage
+    FROM denmark.year_2021_hourly
+    GROUP BY country, zone_id, EXTRACT(HOUR FROM datetime_utc)
+), dk_2022_summary AS (
+    SELECT 
+        '2022' AS year_,
+        country,
+        zone_id,
+        EXTRACT(HOUR FROM datetime_utc) AS hour_grouped,
+        ROUND(SUM(carbon_intensity_direct)::NUMERIC, 2) AS total_direct_carbon_emission,
+        ROUND(SUM(carbon_intensity_lca)::NUMERIC, 2) AS total_lca_carbon_emission,
+        ROUND(AVG(renewable_percentage)::NUMERIC, 2) AS avg_renewable_percentage,
+        ROUND(AVG(low_carbon_percentage)::NUMERIC, 2) AS avg_low_carbon_percentage
+    FROM denmark.year_2022_hourly
+    GROUP BY country, zone_id, EXTRACT(HOUR FROM datetime_utc)
+), dk_2023_summary AS (
+    SELECT 
+        '2023' AS year_,
+        country,
+        zone_id,
+        EXTRACT(HOUR FROM datetime_utc) AS hour_grouped,
+        ROUND(SUM(carbon_intensity_direct)::NUMERIC, 2) AS total_direct_carbon_emission,
+        ROUND(SUM(carbon_intensity_lca)::NUMERIC, 2) AS total_lca_carbon_emission,
+        ROUND(AVG(renewable_percentage)::NUMERIC, 2) AS avg_renewable_percentage,
+        ROUND(AVG(low_carbon_percentage)::NUMERIC, 2) AS avg_low_carbon_percentage
+    FROM denmark.year_2023_hourly
+    GROUP BY country, zone_id, EXTRACT(HOUR FROM datetime_utc)
+), dk_2024_summary AS (
+    SELECT 
+        '2024' AS year_,
+        country,
+        zone_id,
+        EXTRACT(HOUR FROM datetime_utc) AS hour_grouped,
+        ROUND(SUM(carbon_intensity_direct)::NUMERIC, 2) AS total_direct_carbon_emission,
+        ROUND(SUM(carbon_intensity_lca)::NUMERIC, 2) AS total_lca_carbon_emission,
+        ROUND(AVG(renewable_percentage)::NUMERIC, 2) AS avg_renewable_percentage,
+        ROUND(AVG(low_carbon_percentage)::NUMERIC, 2) AS avg_low_carbon_percentage
+    FROM denmark.year_2024_hourly
+    GROUP BY country, zone_id, EXTRACT(HOUR FROM datetime_utc)
+), total_summary AS (
+    SELECT * FROM dk_2021_summary
+    UNION ALL
+    SELECT * FROM dk_2022_summary
+    UNION ALL
+    SELECT * FROM dk_2023_summary
+    UNION ALL
+    SELECT * FROM dk_2024_summary
+), with_differences AS (
+    SELECT 
+        *,
+        LEAD(total_direct_carbon_emission) OVER (PARTITION BY country, zone_id ORDER BY year_, hour_grouped) AS next_direct_carbon_emission,
+        LEAD(total_lca_carbon_emission) OVER (PARTITION BY country, zone_id ORDER BY year_, hour_grouped) AS next_lca_carbon_emission
+    FROM total_summary
+)
+
+SELECT 
+    year_,
+    country,
+    zone_id,
+    hour_grouped,
+    total_direct_carbon_emission,
+    next_direct_carbon_emission,
+    CASE 
+        WHEN next_direct_carbon_emission IS NOT NULL AND total_direct_carbon_emission != 0 
+        THEN ROUND(((next_direct_carbon_emission - total_direct_carbon_emission) / total_direct_carbon_emission) * 100::NUMERIC, 2)
+        ELSE NULL 
+    END AS direct_decrease_or_increase,
+    total_lca_carbon_emission,
+    next_lca_carbon_emission,
+    CASE 
+        WHEN next_lca_carbon_emission IS NOT NULL AND total_lca_carbon_emission != 0 
+        THEN ROUND(((next_lca_carbon_emission - total_lca_carbon_emission) / total_lca_carbon_emission) * 100::NUMERIC, 2)
+        ELSE NULL 
+    END AS lca_decrease_or_increase,
+    avg_renewable_percentage,
+    avg_low_carbon_percentage
+FROM with_differences
+ORDER BY year_, hour_grouped
+
+
